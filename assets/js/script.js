831d32320feb94db0f3a4dd443ba3f15dd433da2
mirindraainah <mirindraainah2@gmail.com>
2023-05-22 16:49:39 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// cacher le modal
+function hideModal() {
+var modal = document.getElementById("myModal");
+modal.style.display = "none";
+}
+
+// Réinitialisation des champs
+function resetInputs() {
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+}
+}
+
+// Événement qui se déclenche lorsque la page est réactualisée
+window.addEventListener('beforeunload', resetInputs);
+
+const form = document.querySelector('form');
+const modal = document.querySelector(".modal");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  modal.style.display = "block";
+});
